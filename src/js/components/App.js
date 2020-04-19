1d060d9a57e8e65adf1eed80a38c3e51cdde4aa9
sowmya dinavahi <sowmya.dsl@gmail.com>
2020-04-19 13:29:05 -0700

    refactor App component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ProductDetail from "./ProductDetail.component";
 import NavBar from "./NavBar.component";
+import CartPage from "./CartPage.component";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const appStyle = {
   display: "flex"
@@ -8,9 +10,18 @@ const appStyle = {
 
 export default function App() {
   return (
-    <div className="App" style={{ appStyle }}>
-      <NavBar />
-      <ProductDetail />
-    </div>
+    <Router>
+      <div className="App" style={{ appStyle }}>
+        <NavBar />
+        <Switch>
+          <Route path="/cart">
+            <CartPage />
+          </Route>
+          <Route path="/">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
