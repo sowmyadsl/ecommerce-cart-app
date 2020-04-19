0826ce268fe0f2e564f402c7026a28ee6cd58fa8
sowmya dinavahi <sowmya.dsl@gmail.com>
2020-04-19 13:28:46 -0700

    update index.js with redux packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/configureStore";
 
 import App from "./js/components/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
