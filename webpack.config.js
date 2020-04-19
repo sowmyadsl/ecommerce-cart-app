a6c9973cbe623fbadd069d71b1264343f0a37cfe
sowmya dinavahi <sowmya.dsl@gmail.com>
2020-04-19 13:26:40 -0700

    update webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require("path");
-
-module.resolves = {
-  alias: {
-    Images: path.resolve(__dirname, "src/public/images/")
+const optimization = {
+  splitChunks: {
+    cacheGroups: {
+      commons: { test: /[\\/]node_modules[\\/]/, name: "common", chunks: "all" }
+    }
   }
 };
+const entry = {
+  ProductDetail: ["./src/js/components/ProductDetail.component.js"]
+};
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "main.js",
+    publicPath: "/"
+  },
+  optimization,
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization"
+    }
+  },
+  resolve: {
+    alias: {
+      Images: path.resolve(__dirname, "src/public/images"),
+      UI: path.resolve(__dirname, "src/js/components/UI"),
+      Components: path.resolve(__dirname, "src/js/components/"),
+      Helpers: path.resolve(__dirname, "src/helpers/"),
+      Actions: path.resolve(__dirname, "src/js/actions/")
+    }
+  },
   module: {
     rules: [
       {
